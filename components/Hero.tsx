dd70d8b0edc56d7b1e2d2cebdcdbcb33932e0d80
DScaife <scaifedan@gmail.com>
2026-04-04 21:53:54 +0100

    made hero h2

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,7 @@ import Typography from "./Typography";
 const Hero = () => {
   return (
     <section className="w-full">
-      <Typography variant="hero">
+      <Typography variant="hero" as={"h2"}>
         This app removes hidden metadata directly in your browser, so your files stay on your device and you can practice good digital
         hygiene.
       </Typography>
