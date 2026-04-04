c8f26be7d22d3dc427de95f8e1e1602d10844679
DScaife <scaifedan@gmail.com>
2026-04-04 06:11:19 +0100

    changed footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,14 +1,17 @@
 const Footer = () => {
   return (
     <footer className="flex items-center h-40">
-      <div className="flex flex-col sm:flex-row justify-between items-center gap-2 w-full py-4 sm:h-14">
-        <p className="text-base">
-          Built for privacy. Source code on{" "}
-          <a className="underline" href="https://github.com/DScaife/privmeta/tree/master" target="_blank" rel="noopener noreferrer">
-            GitHub
-          </a>
-          .
-        </p>
+      <div className="w-full py-(--space-xl) flex flex-col gap-(--space-xl) sm:flex-row sm:justify-between sm:items-end">
+        <p className="text-muted-foreground">Built for everyday digital hygiene.</p>
+
+        <a
+          className="underline underline-offset-4"
+          href="https://github.com/DScaife/privmeta/tree/master"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View source on GitHub
+        </a>
       </div>
     </footer>
   );
