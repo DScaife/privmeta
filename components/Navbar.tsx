61d7f1ec712062a05faf40a17c4e8a016841cc35
DScaife <scaifedan@gmail.com>
2026-04-17 21:46:45 +0100

    new logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,6 +4,7 @@ import { ThemeToggle } from "./ThemeToggle";
 import { Button } from "@/components/ui/button";
 import { Code } from "lucide-react";
 import Link from "next/link";
+import Image from "next/image";
 import Typography from "./Typography";
 
 const BMC = () => (
@@ -31,9 +32,13 @@ const Navbar = () => {
       <nav className="h-40 flex flex-col sm:flex-row gap-(--space-lg) justify-center sm:justify-between items-start sm:items-center">
         <div className="flex justify-between items-center w-full">
           <Link href="/" prefetch={false} aria-label="Go to PrivMeta homepage">
-            <Typography as="span" variant="h1" className="leading-none">
-              PrivMeta
-            </Typography>
+            <span className="flex items-center gap-(--space-sm)">
+              <Image src="/logo-dark.svg" alt="" aria-hidden="true" width={52} height={52} className="block h-1/2 dark:hidden" />
+              <Image src="/logo-light.svg" alt="" aria-hidden="true" width={52} height={52} className="hidden h-1/2 dark:block" />
+              <Typography as="span" variant="h1" className="leading-none">
+                PrivMeta
+              </Typography>
+            </span>
           </Link>
           <div className="flex gap-(--space-md) sm:hidden">
             <Tooltip>
